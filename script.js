0882c23a9ad9674719c0028effb7c12f2accd592
LobstrHandz <67736634+LobstrHandz@users.noreply.github.com>
2025-02-12 12:41:42 +0400

    Added the forewoman

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
   const LIST = [
     { name: 'artur.nyaorim', reason: 'For being a bitch, extracting and leaving teammates behind', date: '10.02.2025' },
     { name: 'That stupid idiot from gas service', reason: 'For being a stupid idiot', date: '11.02.2025' },
-    { name: 'A group of young man on the stairs in front of McDonalds', reason: 'For blocking the entire staircase like stupid idiots', date: '11.02.2025' }
+    { name: 'A group of young man on the stairs in front of McDonalds', reason: 'For blocking the entire staircase like stupid idiots', date: '11.02.2025' },
+    { name: 'The forewoman', reason: 'For being bad at planning and documenting works at my new apartment and causing me a lot of stress', date: '12.02.2025' }
   ];
 
   const listTable = document.querySelector('#list');
