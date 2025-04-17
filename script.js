e8b5bd28af61b6e4139520657155e70d4cdc4ee7
LobstrHandz <67736634+LobstrHandz@users.noreply.github.com>
2025-04-17 19:17:35 +0400

    List updated

--- script.js
+++ script.js
@@ -3,7 +3,9 @@
     { name: 'artur.nyaorim', reason: 'For being a bitch, extracting and leaving teammates behind', date: '10.02.2025' },
     { name: 'That stupid idiot from gas service', reason: 'For being a stupid idiot', date: '11.02.2025' },
     { name: 'A group of young man on the stairs in front of McDonalds', reason: 'For blocking the entire staircase like stupid idiots', date: '11.02.2025' },
-    { name: 'The forewoman', reason: 'For being bad at planning and documenting works at my new apartment and causing me a lot of stress', date: '12.02.2025' }
+    { name: 'The forewoman', reason: 'For being bad at planning and documenting works at my new apartment and causing me a lot of stress', date: '12.02.2025' },
+    { name: 'The manager lady from company that did my windows', reason: 'For being incredibly bad at her job and delaying the installation for 5 months', date: '17.04.2025' },
+    { name: 'Workers from company that did my windows', reason: 'For having ass for a brain', date: '17.04.2025' }
   ];
 
   const listTable = document.querySelector('#list');
